408b685584063c60d6bcc35248190974ff660ff9
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-20 12:36:42 +0200

    Added Footer and Header Components.

---
+++ movie-page/moviehub/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles.css';
+
+export default function Header () {
+    return(
+    
+    <div className='header'>
+        <img className='logo' src='logo.png' alt='Movie Hub'/>
+        <h2> It's time for PopCorn, Find your Favorite movie Here!😀</h2>
+    </div>);
+}
