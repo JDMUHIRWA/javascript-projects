510390248aba857f91db9ec1ea27194986ea60a2
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-21 22:10:42 +0200

    created state

---
+++ movie-page/moviehub/src/components/MoviesGrid.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles.css';
+
+export default function MoviesGrid() {
+    const [movies, setMovies] = React.useState([]);
+    
+
+  return (
+    <div>
+        
+        </div>
+    );
+    }
