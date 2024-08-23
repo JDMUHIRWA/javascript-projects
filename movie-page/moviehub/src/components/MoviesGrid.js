e2ac53efebe09abfdcaaa72eafe4bd7eb9483ad9
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-23 05:40:34 +0200

    refactor: Add genre and rating filters to MoviesGrid component

--- movie-page/moviehub/src/components/MoviesGrid.js
+++ movie-page/moviehub/src/components/MoviesGrid.js
@@ -5,7 +5,9 @@ import MovieCard from "./MoviesCard";
 export default function MoviesGrid() {
   const [movies, setMovies] = React.useState([]);
   const [searchTerm, setSearchTerm] = React.useState("");
-  
+  const [genre, setGenre] = React.useState("All Genres");
+  const [rating, setRating] = React.useState('All Ratings');
+
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
@@ -31,6 +33,28 @@ export default function MoviesGrid() {
         value={searchTerm}
         onChange={handleSearchChange}
       ></input>
+
+      <div className="filter-bar">
+            <div className="filter-slot">
+                <label>Genre</label>
+                <select className="filter-dropdown">
+                    <option value="All Genres">All Genres</option>
+                    <option value="Action">Action</option>
+                    <option value="Comedy">Comedy</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Horror">Horror</option>
+                </select>
+            </div>
+            <div className="filter-slot">
+                <label>Rating</label>
+                <select className="filter-dropdown">
+                <option value="All">All</option>
+                    <option value="Good">Good</option>
+                    <option value="Ok">Ok</option>
+                    <option value="Bad">Bad</option>
+                </select>
+            </div>
+      </div>
       <div className="movies-grid">
         {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
