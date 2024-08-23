54ae9c4aa4b40fc12e31eedf83f55d5fa2d0691f
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-23 02:15:21 +0200

    refactor: Update MoviesGrid component to display movie cards

--- movie-page/moviehub/src/components/MoviesGrid.js
+++ movie-page/moviehub/src/components/MoviesGrid.js
@@ -14,9 +14,15 @@ export default function MoviesGrid() {
             
         }, [])
         return (
-            <div>
-                {movies.length}
-
-                </div>
-            );
-    }
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <div className="movie-card" key={movie.id}>
+                        <img src={`images/${movie.image}`} alt={movie.title} />
+                        <h3 className="movie-card-title">{movie.title}</h3>
+                        <p className="movie-card-genre">{movie.genre}</p>
+                        <p className="movie-card-rating">{movie.rating}</p>
+                    </div>
+                ))}
+            </div>
+        );
+}
