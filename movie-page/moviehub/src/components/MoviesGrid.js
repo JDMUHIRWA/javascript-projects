fdfcd2bdb98f3915a1e487207c2eb9cc49e83fe5
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-23 05:13:05 +0200

    refactor: Update MoviesGrid component to use searchTerm for movie filtering

--- movie-page/moviehub/src/components/MoviesGrid.js
+++ movie-page/moviehub/src/components/MoviesGrid.js
@@ -4,7 +4,7 @@ import MovieCard from "./MoviesCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = React.useState([]);
-  const [search, setSearchTerm] = React.useState("");
+  const [searchTerm, setSearchTerm] = React.useState("");
   useEffect(() => {
     fetch("movies.json")
       .then((response) => response.json())
@@ -12,11 +12,26 @@ export default function MoviesGrid() {
         setMovies(data);
       });
   }, []);
+
+    const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    };
+
+    const filteredMovies = movies.filter((movie) => 
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    
   return (
     <div>
-      <input type="text" placeholder="Search movies.." className="search-input"></input>
+      <input
+        type="text"
+        placeholder="Search movies.."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      ></input>
       <div className="movies-grid">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard movie={movie} key={movie.id} />
         ))}
       </div>
