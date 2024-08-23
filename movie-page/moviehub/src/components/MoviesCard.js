a1023c76b1e077a86eba2e13e8d24beec07bc356
JDMUHIRWA <j.hareriman@alustudent.com>
2024-08-23 04:12:43 +0200

    refactor: Update MoviesGrid and MovieCard components

--- movie-page/moviehub/src/components/MoviesCard.js
+++ movie-page/moviehub/src/components/MoviesCard.js
@@ -1,14 +1,20 @@
 import React from "react";
-import '../styles.css';
+import "../styles.css";
 
 export default function MovieCard({ movie }) {
-    return (
-
-        <div className="movie-card" key={movie.id}>
-            <img src={`images/${movie.image}`} alt={movie.title} />
-            <h3 className="movie-card-title">{movie.title}</h3>
-            <p className="movie-card-genre">{movie.genre}</p>
-            <p className="movie-card-rating">{movie.rating}</p>
-        </div>
-    );
+  const handleError = (e) => {
+    e.target.src = "images/default.png";
+  };
+  return (
+    <div className="movie-card" key={movie.id}>
+      <img
+        src={`images/${movie.image}`}
+        alt={movie.title}
+        onError={handleError}
+      />
+      <h3 className="movie-card-title">{movie.title}</h3>
+      <p className="movie-card-genre">{movie.genre}</p>
+      <p className="movie-card-rating">{movie.rating}</p>
+    </div>
+  );
 }
--- movie-page/moviehub/src/components/MoviesGrid.js
+++ movie-page/moviehub/src/components/MoviesGrid.js
@@ -1,24 +1,22 @@
 import React, { useEffect } from "react";
-import '../styles.css';
-import MovieCard from './MoviesCard';
+import "../styles.css";
+import MovieCard from "./MoviesCard";
 
 export default function MoviesGrid() {
-    const [movies, setMovies] = React.useState([]);
+  const [movies, setMovies] = React.useState([]);
 
-        
-        useEffect (() => {
-            fetch('movies.json')
-            .then(response => response.json())
-            .then(data => {
-                setMovies(data);
-            })
-            
-        }, [])
-        return (
-            <div className="movies-grid">
-                {movies.map(movie => (
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))}
-            </div>
-        );
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  }, []);
+  return (
+    <div className="movies-grid">
+      {movies.map((movie) => (
+        <MovieCard movie={movie} key={movie.id} />
+      ))}
+    </div>
+  );
 }
